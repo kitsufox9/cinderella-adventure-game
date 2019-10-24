4e3ef3f3182bc6d0f1594294d26d191ede58abb6
kitsufox9 <christinafox93@gmail.com>
2019-10-24 01:15:33 -0400

    added components

---
+++ src/components/routes/StartHere.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class StartHere extends Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default StartHere;
